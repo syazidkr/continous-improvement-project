ad31fb3b7070bab67df0ff0fbb901cff4ff25cb0
syazidkr <111582922+syazidkr@users.noreply.github.com>
2025-09-18 15:38:10 +0000

    modify several files

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
   return (
    <div className="min-h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 ">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image src="/logo.png" alt="logo" width={32} height={32}></Image> 
           <span className="hidden lg:block font-bold">SchooLama</span>      
@@ -18,7 +18,6 @@ export default function DashboardLayout({
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll flex flex-col">
-
         {children}
       </div>
    </div>
